f1ddecb91ec8625aba8536aa7bb69ae42d602d60
demos <dtsellos03@gmail.com>
2019-06-24 19:16:08 -0700

    Restrict angles

--- public/javascripts/3dcode.js
+++ public/javascripts/3dcode.js
@@ -66,6 +66,7 @@ function createControls() {
 
     controls = new THREE.OrbitControls( camera, container );
     controls.maxPolarAngle = 8*Math.PI/10;
+    controls.minPolarAngle = 2.5*Math.PI/10;
     controls.minAzimuthAngle = -5*Math.PI/10;
     controls.maxAzimuthAngle = 5*Math.PI/10;
 
